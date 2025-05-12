d07e298a16507522caf94935f12b99f56acc15f4
André Rogério Bononi <arbononi@gmail.com>
2025-05-12 00:09:22 -0300

    novos ajustes

--- controllers/narrativa_controller.py
+++ controllers/narrativa_controller.py
@@ -1,4 +1,4 @@
-from database.banco import Banco
+# from database.banco import Banco
 from views.capitulos_view import CapitulosView
 from utils.userfunctions import exibir_mensagem
 
@@ -14,99 +14,99 @@ class NarrativaController:
 
     def iniciar(self):
         self._view.iniciar()
+        _ = exibir_mensagem("Pressione qualquer tecla para continuar...", wait_key=True)
+        # while True:
+            # # cap = Banco._model.get_capitulo(self.capitulo_atual)
+            # conteudo = cap.obter_linha(self.linha_atual)
+
+            # if isinstance(conteudo, dict):
+            #     texto = conteudo.get('texto', '')
+            #     opcoes = conteudo.get('opcoes', [])
+            # else:
+            #     texto = conteudo
+            #     opcoes = []
+
+            # self._view.exibir_narrativa(texto)
+            # self._view.mostrar_opcoes(opcoes)
+
+            # if not opcoes:
+            #     proxima = self.proxima_linha(self.linha_atual, cap.linhas)
+            #     if proxima is None:
+            #         exibir_mensagem("Fim do capítulo!", wait_key=True)
+            #         break
+            #     self.linha_atual = proxima
+            #     continue
+
+            # entrada = self._view.solicitar_acao()
+
+            # try:
+            #     escolha = int(entrada) - 1
+            #     if 0 <= escolha < len(opcoes):
+            #         acao = opcoes[escolha]['acao']
+            #         if isinstance(acao, dict):
+            #             auto = acao.get("auto")
+            #             destino = acao.get("destino")
+            #             proxima = acao.get('proximo', self.linha_atual)
+            #             if auto:
+            #                 if destino:
+            #                    argumentos = self.get_argumentos(destino, proxima, cap)
+            #                 self.executar_acao_automatica(auto, argumentos)
+            #             if acao['tipo'] == 'pular':
+            #                 self.linha_atual = acao.get('proximo', self.linha_atual)
+            #                 continue
+            #             if destino:
+            #                 idx = cap.listar_linhas().index(destino)
+            #                 fim = cap.listar_linhas().index(proxima)
+            #                 for i in range(idx, fim):
+            #                     linha = cap.obter_linha(cap.listar_linhas()[i])
+            #                     if isinstance(linha, dict):
+            #                         texto = linha.get('texto', '')
+            #                         opcoes = linha.get('opcoes', [])
+            #                     else:
+            #                         texto = linha
+            #                         opcoes = []
+            #                     self._view.exibir_narrativa(texto)
+            #                 self.linha_atual = proxima
+            #         else:
+            #             conteudo = cap.obter_linha(acao)
+            #             if isinstance(conteudo, dict):
+            #                 texto = conteudo.get('texto', '')
+            #                 opcoes = conteudo.get('opcoes', [])
+            #             else:
+            #                 texto = conteudo
+            #                 opcoes = []
+            #             self.view.exibir_narrativa(texto)
+            #     else:
+            #         _ = exibir_mensagem("Escolha inválida! Tente novamente!", wait_key=True)
+            # except ValueError:
+            #     _ = exibir_mensagem("Entrada inválida! Escolha o número da opção", wait_key=True)
+
+    # def proxima_linha(self, atual, linhas):
+    #     chaves = list(linhas.keys())
+    #     try:
+    #         idx = chaves.index(atual)
+    #         return chaves[idx + 1] if idx + 1 < len(chaves) else None
+    #     except ValueError:
+    #         return None
         
-        while True:
-            cap = Banco._model.get_capitulo(self.capitulo_atual)
-            conteudo = cap.obter_linha(self.linha_atual)
-
-            if isinstance(conteudo, dict):
-                texto = conteudo.get('texto', '')
-                opcoes = conteudo.get('opcoes', [])
-            else:
-                texto = conteudo
-                opcoes = []
-
-            self._view.exibir_narrativa(texto)
-            self._view.mostrar_opcoes(opcoes)
-
-            if not opcoes:
-                proxima = self.proxima_linha(self.linha_atual, cap.linhas)
-                if proxima is None:
-                    exibir_mensagem("Fim do capítulo!", wait_key=True)
-                    break
-                self.linha_atual = proxima
-                continue
-
-            entrada = self._view.solicitar_acao()
-
-            try:
-                escolha = int(entrada) - 1
-                if 0 <= escolha < len(opcoes):
-                    acao = opcoes[escolha]['acao']
-                    if isinstance(acao, dict):
-                        auto = acao.get("auto")
-                        destino = acao.get("destino")
-                        proxima = acao.get('proximo', self.linha_atual)
-                        if auto:
-                            if destino:
-                               argumentos = self.get_argumentos(destino, proxima, cap)
-                            self.executar_acao_automatica(auto, argumentos)
-                        if acao['tipo'] == 'pular':
-                            self.linha_atual = acao.get('proximo', self.linha_atual)
-                            continue
-                        if destino:
-                            idx = cap.listar_linhas().index(destino)
-                            fim = cap.listar_linhas().index(proxima)
-                            for i in range(idx, fim):
-                                linha = cap.obter_linha(cap.listar_linhas()[i])
-                                if isinstance(linha, dict):
-                                    texto = linha.get('texto', '')
-                                    opcoes = linha.get('opcoes', [])
-                                else:
-                                    texto = linha
-                                    opcoes = []
-                                self._view.exibir_narrativa(texto)
-                            self.linha_atual = proxima
-                    else:
-                        conteudo = cap.obter_linha(acao)
-                        if isinstance(conteudo, dict):
-                            texto = conteudo.get('texto', '')
-                            opcoes = conteudo.get('opcoes', [])
-                        else:
-                            texto = conteudo
-                            opcoes = []
-                        self.view.exibir_narrativa(texto)
-                else:
-                    _ = exibir_mensagem("Escolha inválida! Tente novamente!", wait_key=True)
-            except ValueError:
-                _ = exibir_mensagem("Entrada inválida! Escolha o número da opção", wait_key=True)
-
-    def proxima_linha(self, atual, linhas):
-        chaves = list(linhas.keys())
-        try:
-            idx = chaves.index(atual)
-            return chaves[idx + 1] if idx + 1 < len(chaves) else None
-        except ValueError:
-            return None
-        
-    def get_argumentos(self, destino, proxima, cap):
-        idx = cap.listar_linhas().index(destino)
-        fim = cap.listar_linhas().index(proxima)
-        args = []
-        for i in range(idx, fim):
-            linha = cap.obter_linha(cap.listar_linhas()[i])
-            if linha:
-               args.append(linha)
-        return args
+    # def get_argumentos(self, destino, proxima, cap):
+    #     idx = cap.listar_linhas().index(destino)
+    #     fim = cap.listar_linhas().index(proxima)
+    #     args = []
+    #     for i in range(idx, fim):
+    #         linha = cap.obter_linha(cap.listar_linhas()[i])
+    #         if linha:
+    #            args.append(linha)
+    #     return args
             
-    def executar_acao_automatica(self, nome_acao, args=None):
-        if isinstance(nome_acao, list):
-            for nome in nome_acao:
-                self.executar_acao_automatica(nome, args)
-            return
+    # def executar_acao_automatica(self, nome_acao, args=None):
+    #     if isinstance(nome_acao, list):
+    #         for nome in nome_acao:
+    #             self.executar_acao_automatica(nome, args)
+    #         return
         
-        acao = self.acoes_automatcias.get(nome_acao)
-        if callable(acao):
-            acao(args)
-        else:
-            exibir_mensagem(f"[WARN] Ação automática desconhecida: {nome_acao}")
+    #     acao = self.acoes_automatcias.get(nome_acao)
+    #     if callable(acao):
+    #         acao(args)
+    #     else:
+    #         exibir_mensagem(f"[WARN] Ação automática desconhecida: {nome_acao}")
--- game1984.py
+++ game1984.py
@@ -10,11 +10,9 @@ from os import system as clear_screen
 from utils import userfunctions
 from layouts.layouts import tela_principal, rosto, tela_capitulos, legenda_rosto
 from config import lin_terminal, col_terminal, lin_message
-from database.banco import Banco
 from controllers.narrativa_controller import NarrativaController
 
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-_banco = Banco()
 
 def set_size_terminal(lines=30, columns=100):
     os.system(f"mode con: cols={columns} lines={lines}")
@@ -24,18 +22,9 @@ def iniciar():
     str_data_atual = userfunctions.formatar_data(userfunctions.get_data_atual(), True, True)
     userfunctions.exibir_conteudo(str_data_atual, lin=2, col=104)
     _ = userfunctions.exibir_mensagem("Aguarde... Carregando Roteiros...")
-    narrativa_controller = NarrativaController()
     userfunctions.limpar_linha()
-    try:
-        _banco.carregar_arquivo("script_capitulo_1.yaml")
-        narrativa_controller.iniciar()
-        
-    except FileNotFoundError as notFound:
-        _ = userfunctions.exibir_mensagem(notFound, wait_key=True)
-        return
-    except Exception as ex:
-        _ = userfunctions.exibir_mensagem(ex, wait_key=True)
-
+    _app = NarrativaController()
+    _app.iniciar()
 
 if __name__ == "__main__":
     os.system("chcp 65001 > nul")
